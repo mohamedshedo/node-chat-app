96275b9a9136e1a32370eafbd7796422259114fc
MohamedShedo <mohamedshedo@gmail.com>
2018-10-04 03:31:33 +0200

    adding scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -9,23 +9,31 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    let formattedTime = moment(message.createdAt).format('h:mm a');
-  console.log('newMessage', message);
-  var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${formattedTime} ${message.text}`);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
+  scrollToButtom();
 });
 
 socket.on('newLocationMessage', function (message) {
     let formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
+ let template=jQuery('#location-message-template').html();
+ let html= Mustache.render(template,{
+   createdAt:formattedTime,
+   url:message.url,
+   from:message.from
+ });
 
-  li.text(`${message.from}: ${formattedTime} `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+
+ jQuery('#messages').append(html);
+ scrollToButtom();
+
 });
 
 jQuery('#message-form').on('submit', function (e) {
@@ -38,6 +46,20 @@ let messageTextBox =jQuery('[name=message]');
    messageTextBox.val('')
   });
 });
+function scrollToButtom(){
+  let messages = jQuery('#messages');
+  let newMessage= messages.children('li:last-child');
+  let clientHeight= messages.prop('clientHeight');
+  let scrollHeight = messages.prop('scrollHeight');
+  let scrollTop= messages.prop('scrollTop');
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight=newMessage.prev().innerHeight();
+
+
+  if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
 
 var locationButton = jQuery('#send-location');
 locationButton.on('click', function () {
